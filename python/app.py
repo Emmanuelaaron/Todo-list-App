5365030da4f3ec5e351ea96268b9ef4939204ba6
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2018-09-20 13:48:34 +0300

    Modifies #160611374

--- python/app.py
+++ python/app.py
@@ -6,16 +6,29 @@ from accounts import *
 if __name__ == "__main__":
     name = input("Enter your name:")
     password = input("Enter your password:")
-    for key in accounts:
-        if key is name and accounts[key] is password:
-            login(name, password)
+    add_account(name, password)
+    while login(name, password):
+        print ("\nSelect Option")
+        print ("1. Creat task")
+        print ("2. delete task")
+        print ("3. delete all tasks")
+        print ("4. mark task finished\n")
+        select = int(input("Selection: "))
+        if select is 1:
+            task = str(input("Enter task to be added"))
+            print (create_task(task))
+            continue
+        elif select is 2:
+            task = str(input("Enter task to be removed"))
+            print (delete_task(task))
+            continue
+        elif select is 3:
+            print (delete_all_tasks())
+            continue
+        elif select is 4:
+            task = str(input("Enter task finished"))
+            print (mark_as_finished(task))
+            continue
         else:
-            add_account(name, password)
-    print ("Select Option:")
-    print ("1. Create task ")
-    print ("2. delete task")
-    print ("3. delete all tasks")
-    print ("4. mark task finished")
-    select = input("selection: ")
-
+            exit
 
--- python/tasks.py
+++ python/tasks.py
@@ -4,26 +4,29 @@ def create_task(task):
     """
     Adds the task (string value) to todo_list
     """
-    
-    return todo_list.append(task)
+    todo_list.append(task)
+    return todo_list
 
 #function that deletes the task
 def delete_task(task):
     """
     Removes the specified task from the todo_list
     """
-    return todo_list.remove(task)
+    todo_list.remove(task)
+    return todo_list
 
 #function that marks a task finiished
 def mark_as_finished(task):
     """
     Appends the string lable '[finished]' at the end of the task
     """
-    return task + " [finished]"
+    res = task + " [finished]"
+    return res
 
 #function that deletes all tasks
 def delete_all_tasks():
     """
     deletes all tasks from the todo_list
     """
-    return todo_list.clear()
+    todo_list.clear()
+    return todo_list
